10b58498eaf740a2253ed699c86df61322e38587
Htet Linn Oo <hloo@onesmartstar.com.mm>
2019-12-24 13:53:51 +0630

    app.js changed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const orderRoutes = require('./api/routes/orders')
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.static('uploads'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));

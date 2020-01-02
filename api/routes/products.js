e82d20f16b3289fb16995e523ae685acfff53906
Htet Linn Oo <hloo@onesmartstar.com.mm>
2020-01-02 15:38:52 +0630

    middleware added

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,19 +3,20 @@ const router = express.Router();
 
 const imgUploadHelper = require('../Helpers/ImageUploadHelper');
 const ProductController = require('../Controllers/ProductController');
+const checkAuth = require('../Middleware/check-auth');
 
 //ROUTES
 //get all products route
 router.get('/', ProductController.index)
 
 //product save route
-router.post('/',imgUploadHelper.single('productImg'), ProductController.store)
+router.post('/',checkAuth, imgUploadHelper.single('productImg'), ProductController.store)
 
 //product update route
-router.patch('/:product_id', ProductController.update)
+router.patch('/:product_id', checkAuth, ProductController.update)
 
 //product delete route
-router.delete('/:product_id', ProductController.delete)
+router.delete('/:product_id', checkAuth, ProductController.delete)
 
 
 module.exports = router;
